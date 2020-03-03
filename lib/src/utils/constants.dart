e92cdfddb7093633d6aae66e091ec079a38f0ea1
Christophe Bonello <cbonello@users.noreply.github.com>
2020-03-03 11:02:26 -0500

    remove unused constants

--- lib/src/utils/constants.dart
+++ lib/src/utils/constants.dart
@@ -1,9 +1,5 @@
 const String APP_NAME = 'Starter App';
-const String APP_COPYRIGHT = '©2020, Christophe Bonello';
 const String APP_ASSET_LOGO = 'assets/images/app_logo.png';
 
 const int MIN_PASSWORD_LENGTH = 8;
 const double MIN_PASSWORD_STRENGTH = 0.5;
-
-// Cloud Firestore.
-const String USERS_PATH = 'users';
