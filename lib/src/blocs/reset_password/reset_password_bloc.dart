4e9d5c8f0a2f514bf70b9c90a0061913f84f0be5
Christophe Bonello <cbonello@users.noreply.github.com>
2020-04-10 10:03:17 -0400

    add debouncing

--- lib/src/blocs/reset_password/reset_password_bloc.dart
+++ lib/src/blocs/reset_password/reset_password_bloc.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/foundation.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:starter_app/src/repositories/authentication_repository.dart';
 import 'package:starter_app/src/utils/exceptions.dart';
 import 'package:starter_app/src/utils/validators.dart';
@@ -18,6 +19,22 @@ class ResetPasswordBloc extends Bloc<ResetPasswordEvent, ResetPasswordState> {
 
   @override
   ResetPasswordState get initialState => ResetPasswordState.empty();
+
+  @override
+  Stream<ResetPasswordState> transformEvents(
+    Stream<ResetPasswordEvent> events,
+    Stream<ResetPasswordState> Function(ResetPasswordEvent event) next,
+  ) {
+    final Stream<ResetPasswordEvent> nonDebounceStream = events
+        .where((ResetPasswordEvent event) => event is! EmailChangedResetPasswordEvent);
+    final Stream<ResetPasswordEvent> debounceStream = events
+        .where((ResetPasswordEvent event) => event is EmailChangedResetPasswordEvent)
+        .debounceTime(const Duration(milliseconds: 300));
+    return super.transformEvents(
+      nonDebounceStream.mergeWith(<Stream<ResetPasswordEvent>>[debounceStream]),
+      next,
+    );
+  }
 
   @override
   Stream<ResetPasswordState> mapEventToState(
