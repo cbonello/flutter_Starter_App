dd204dbcb539370478e64d7e58c061298ff3aeb0
Christophe Bonello <cbonello@users.noreply.github.com>
2020-04-22 13:22:58 -0400

    add widget testing for reset password screen

---
+++ test/widgets/reset_password_screen_test.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/src/blocs/authentication/authentication_bloc.dart';
+import 'package:flutter_auth/src/configuration.dart';
+import 'package:flutter_auth/src/keys.dart';
+import 'package:flutter_auth/src/repositories/authentication_repository.dart';
+import 'package:flutter_auth/src/services/analytics.dart';
+import 'package:flutter_auth/src/services/local_storage.dart';
+import 'package:flutter_auth/src/ui/common/index.dart';
+import 'package:flutter_auth/src/ui/reset_password/screen.dart';
+import 'package:flutter_auth/src/utils/app_localizations.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../mock/repositories.dart';
+import '../mock/services.dart';
+
+void main() {
+  final LocalStorageService localStorageServiceMock = MockLocalStorageService();
+  final AuthenticationRepository authRepositoryMock = MockAuthenticationRepository();
+  final AnalyticsService analyticsServiceMock = MockAnalyticsService();
+
+  group('Reset Password screen', () {
+    testWidgets(
+      'Reset button should be disabled by default',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          MultiBlocProvider(
+            providers: <BlocProvider<dynamic>>[
+              BlocProvider<AuthenticationBloc>(
+                create: (BuildContext _) {
+                  return AuthenticationBloc(
+                    localStorageService: localStorageServiceMock,
+                    authRepository: authRepositoryMock,
+                    analyticsService: analyticsServiceMock,
+                  );
+                },
+              ),
+            ],
+            child: MaterialApp(
+              localizationsDelegates: <LocalizationsDelegate<dynamic>>[
+                const AppLocalizationsDelegate(),
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: kSupportedLanguages,
+              home: ResetPasswordScreen(authRepository: authRepositoryMock),
+            ),
+          ),
+        );
+        await tester.pumpAndSettle();
+
+        final Finder buttonFinder =
+            find.byKey(AppWidgetKeys.keys['PasswordResetSubmitButton']);
+        expect(buttonFinder, findsOneWidget);
+        expect(tester.widget<GradientButton>(buttonFinder).enabled, isFalse);
+      },
+    );
+
+    testWidgets(
+      'Reset button should be enabled when email is set',
+      (WidgetTester tester) async {
+        await tester.pumpWidget(
+          MultiBlocProvider(
+            providers: <BlocProvider<dynamic>>[
+              BlocProvider<AuthenticationBloc>(
+                create: (BuildContext _) {
+                  return AuthenticationBloc(
+                    localStorageService: localStorageServiceMock,
+                    authRepository: authRepositoryMock,
+                    analyticsService: analyticsServiceMock,
+                  );
+                },
+              ),
+            ],
+            child: MaterialApp(
+              localizationsDelegates: <LocalizationsDelegate<dynamic>>[
+                const AppLocalizationsDelegate(),
+                GlobalMaterialLocalizations.delegate,
+                GlobalWidgetsLocalizations.delegate,
+                GlobalCupertinoLocalizations.delegate,
+              ],
+              supportedLocales: kSupportedLanguages,
+              home: ResetPasswordScreen(authRepository: authRepositoryMock),
+            ),
+          ),
+        );
+        await tester.pumpAndSettle();
+
+        await tester.enterText(
+          find.byKey(AppWidgetKeys.keys['PasswordResetEmailField']),
+          'john.doe@gmail.com',
+        );
+        await tester.pumpAndSettle();
+
+        final Finder buttonFinder =
+            find.byKey(AppWidgetKeys.keys['PasswordResetSubmitButton']);
+        expect(buttonFinder, findsOneWidget);
+        expect(tester.widget<GradientButton>(buttonFinder).enabled, isTrue);
+      },
+    );
+  });
+}
