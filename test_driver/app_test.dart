b6a1c3b44255965b97f5c91a69b2b4df10c2649f
Christophe Bonello <cbonello@users.noreply.github.com>
2020-04-28 11:33:08 -0400

    fix integration tests

--- test_driver/app_test.dart
+++ test_driver/app_test.dart
@@ -3,10 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   group('Counter App', () {
-    // final SerializableFinder homeCounterField = find.byValueKey('HomeCounterField');
-    final SerializableFinder signInEmailField = find.byValueKey('SignInEmailField');
-    final SerializableFinder signInPasswordField = find.byValueKey('SignInPasswordField');
-    final SerializableFinder signInSubmitButton = find.byValueKey('SignInSubmitButton');
+    final SerializableFinder splashImage = find.byValueKey('SplashImage');
 
     FlutterDriver driver;
     setUpAll(() async {
@@ -14,27 +11,16 @@ void main() {
     });
 
     tearDownAll(() async {
-      if (driver != null) {
-        driver.close();
-      }
+      driver?.close();
     });
 
     test('check flutter driver health', () async {
       final Health health = await driver.checkHealth();
-      expect(health, HealthStatus.ok);
+      expect(health.status, HealthStatus.ok);
     });
 
-    // test('should sign-in successfully', () async {
-    //   driver.requestData("signin");
-
-    //   await driver.tap(signInEmailField);
-    //   await driver.enterText('a@b.com');
-    //   await driver.waitFor(find.text('a@b.com'));
-    //   await driver.tap(signInPasswordField);
-    //   await driver.enterText('abcom123456');
-    //   await driver.waitFor(find.text('abcom123456'));
-    //   await driver.tap(signInSubmitButton);
-    //   expect(true, true);
-    // });
+    test('splash screen should be displayed', () async {
+      await driver.waitFor(splashImage);
+    });
   });
 }
